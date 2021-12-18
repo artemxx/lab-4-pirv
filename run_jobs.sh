cb0c6783afdfd9b0832fbbdea7a0cab0b6b674e4
Artem Garkavy <artem2906x@gmail.com>
2021-12-18 22:49:34 +0300

    add run script

---
+++ run_jobs.sh
@@ -0,0 +1,22 @@
+set -x -e
+
+input_dir="$1"
+filter_file="$2"
+
+mkdir -p my_classes
+CLASSPATH=$(hadoop classpath) javac -d my_classes WordFilter.java
+
+cd my_classes
+jar cvf x.jar *
+mv x.jar ..
+cd ..
+
+hdfs dfs -mkdir -p /user/root/
+hdfs dfs -rm -r -f /user/root/input
+hdfs dfs -rm -r -f /user/root/output
+hdfs dfs -put -f "${input_dir}" /user/root/input
+
+hadoop jar x.jar WordFilter input output "${filter_file}"
+
+hdfs dfs -get -f /user/root/out/* .
+cat part-r-00000
